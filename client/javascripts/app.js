070dc03bdac11329749992cf19508e828e69cd66
francois <frxpoulin@gmail.com>
2014-03-22 11:56:33 -0400

    concat and list all event

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,3 +1,19 @@
-function HomeController($scope){    
-    $scope.chiens = "loll";
+function HomeController($scope, $http){    
+    var datasets = ['Quebec', 'Gatineau', 'Montreal', 'Sherbrook'];
+    $scope.events = [];
+
+    var getData = function(dataset){
+        $http.get("/client/javascripts/data/" + dataset + ".json")
+            .success(function(data){
+                var eventsArray = data.EVTS.EVT;
+                $scope.events = $scope.events.concat(eventsArray);
+            })
+            .error(function(){
+                console.log("error");
+            });
+        };
+
+    for(var i in datasets){
+        getData(datasets[i]);
+    }
 }
