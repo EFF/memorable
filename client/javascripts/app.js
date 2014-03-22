ab15983b1152d79406447243d69f8235fdcbcf3d
Sylvain Perron <sylvain.perron@protorisk.com>
2014-03-22 13:00:16 -0400

    liste des categories

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,7 +1,8 @@
 function HomeController($scope, $http){    
     var datasets = ['Quebec', 'Gatineau', 'Montreal', 'Sherbrook'];
     $scope.events = [];
-
+    $scope.categories = [];
+    
     var getData = function(dataset){
         $http.get("/client/javascripts/data/" + dataset + ".json")
             .success(function(data){
@@ -16,4 +17,14 @@ function HomeController($scope, $http){
     for(var i in datasets){
         getData(datasets[i]);
     }
+    
+    $scope.$watch('events', function() {
+        $scope.categories = window.Enumerable
+                                  .From($scope.events)
+                                  .SelectMany(function(x) {
+                                      return (typeof x.CATEG === 'object' ? x.CATEG : [x.CATEG])
+                                  }).Distinct().ToArray();
+        
+        console.log($scope.categories);
+    });
 }
