da27f4b63ae4657493baae8c4cde5835535153fa
Sylvain Perron <sylvain.perron@protorisk.com>
2014-03-22 13:23:37 -0400

    Added categories dic

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,5 +1,62 @@
 function HomeController($scope, $http){    
-    var datasets = ['Quebec', 'Gatineau', 'Montreal', 'Sherbrook'];
+    var datasets = ['Quebec', 'Gatineau', 'Montreal', 'Sherbrook'],
+        
+            categories_index: {
+                0: "Arts",
+                1: "Arts visuels",
+                2: "Atelier",
+                3: "Chant",
+                4: "Musique",
+                5: "Activité petit budget",
+                6: "Conférence",
+                7: "Visite guidée",
+                8: "Activité littéraire",
+                9: "Exposition",
+                10: "Patrimoine",
+                11: "Théâtre",
+                12: "Activité familiale",
+                13: "Bibliothèques",
+                14: "Jeune public",
+                15: "Grand événement",
+                16: "Sports",
+                17: "Vieux-Québec",
+                18: "Animation",
+                19: "Pâques",
+                20: "Artisanat",
+                21: "Activités estivales",
+                22: "Cirque",
+                23: "Noël",
+                24: "Parc Linéaire",
+                25: "Évenement",
+                26: "Contes",
+                27: "Humour",
+                28: "Relâche",
+                29: "Danse",
+                30: "Divers",
+                31: "Photographie",
+                32: "Multimédia",
+                33: "Littérature",
+                34: "Cinéma",
+                35: "Histoire animée",
+                36: "Spectacles en salles et théâtre",
+                37: "Fêtes et festivals",
+                38: "Galerie d'art et exposition",
+                39: "Expositions",
+                40: "Activités et animations",
+                41: "Spectacles",
+                42: "Salons",
+                43: "Salons et expositions",
+                44: "Événements sportifs"
+            },
+            
+        moods = {
+            'Intello': [], 
+            'Énergétique' : [], 
+            'Festif' : [], 
+            'Créatif' : [], 
+            'Chill': []
+        };
+    
     $scope.events = [];
     $scope.categories = [];
     
