f85ca2f88242b0cc194a246d87e76f491ed9e3e9
Jacques-Olivier D. Bernier <jdesjardinsbernier@gmail.com>
2014-03-22 14:04:29 -0400

    theme with working date

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,3 +1,22 @@
-function HomeController($scope){    
-    $scope.chiens = "loll";
+function HomeController($scope) {
+
+    var dayOfTheWeek = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    var monthsOfTheYear = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+
+    var setCurrentDateTo = function (date) {
+        $scope.currentDate = date;
+        $scope.currentDateAsString = dayOfTheWeek[$scope.currentDate.getDay()] + ' ' + $scope.currentDate.getDate() + ' ' + monthsOfTheYear[$scope.currentDate.getMonth()];
+    };
+
+    $scope.today = function () {
+        setCurrentDateTo(new Date());
+    };
+
+    $scope.tomorrow = function () {
+        setCurrentDateTo(new Date(new Date().getTime() + 24 * 60 * 60 * 1000));
+    };
+
+    $scope.today();
+
 }
