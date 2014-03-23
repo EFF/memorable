53a0a7f99efcaa7085c16bdc487c1c452e144c55
Sylvain Perron <sylvain.perron@protorisk.com>
2014-03-23 13:46:42 -0400

    Handles location detection denial

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -80,16 +80,19 @@ app.controller('HomeController', function ($scope, $http) {
     $scope.events = [];
     $scope.filteredEvents = [];
 
-    var myLongitude, myLatitude, eventbriteEvents = [];
+    var myLongitude, myLatitude, locationSet = false, eventbriteEvents = [];
 
     var getGeolocation = function (location) {
         myLongitude = location.coords.longitude;
         myLatitude = location.coords.latitude;
+        locationSet = true;
         getNearbyEventsOnEventBrite();
     };
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getGeolocation);
+        navigator.geolocation.getCurrentPosition(getGeolocation, function() {
+            locationSet = false;
+        });
     }
 
     
@@ -277,7 +280,9 @@ app.controller('HomeController', function ($scope, $http) {
 
             var events = getEventsByDate(eventsByMood);
 
-            events = filterByLocation(events);
+            if(locationSet) {
+                events = filterByLocation(events);
+            }
 
             for (var i in eventbriteEvents) {
                 events.push(eventbriteEvents[i]);
