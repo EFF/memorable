7b33819fad695983cfc29b264acf96b969dc7663
Sylvain Perron <sylvain.perron@protorisk.com>
2014-03-23 00:54:53 -0400

    distance api

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -77,6 +77,17 @@ function HomeController($scope, $http) {
         navigator.geolocation.getCurrentPosition(getGeolocation);
     }
 
+    var getDistance = function (lat1, lon1, lat2, lon2) {
+        var R = 6371; // km
+        var dLat = (lat2 - lat1).toRad();
+        var dLon = (lon2 - lon1).toRad();
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+    };
+
     var getNearbyEventsOnEventBrite = function () {
 
         Eventbrite({ 'app_key': 'NSE6URISYN4F6UWFB6', 'user_key': null }, function (eb_client) {
